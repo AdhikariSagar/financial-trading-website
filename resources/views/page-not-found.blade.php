33c25572997ee06316a21117f49291b0c83777c5
Sagar Adhikari <sagar.adhikari86@gamil.com>
2024-06-07 00:20:45 +0200

    Add page-not-found route and view

---
+++ resources/views/page-not-found.blade.php
@@ -0,0 +1,5 @@
+<x-main-layout>
+    <div class="fixed h-full w-full bg-slate-100 top-0 left-0 flex justify-center items-center">
+        <div class="text-gray-400">404 | Page not found</div>
+    </div>
+</x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExchangeController;
 use App\Http\Controllers\DashboardController;
@@ -11,7 +12,7 @@ Route::get('/', [DashboardController::class, 'index'])->name('home');
 // Views
 Route::get('/stock',[StockController::class,'home'])->name('stock');
 Route::get('/stock-details/{id}',[StockController::class,'stockDetails'])->name('stock-details');
-
+Route::get('/instrument-details/{type}/{symbol}',[Controller::class,'instrumentDetails'])->name('instrument-details');
 
 Route::get('/cryptocurrency',function(){ return view('cryptocurrency');})->name('cryptocurrency');
 Route::get('/etc',function(){ return view('exchage-traded-commodity');})->name('etc');
@@ -26,6 +27,7 @@ Route::get('/ethereum', function () {return view('ethereum');})->name('ethereum'
 Route::get('/exchange',[ExchangeController::class,'getAll'])->name('exchange');
 Route::get('exchange-details/{symbol}',[ExchangeController::class,'exchangeDetail'])->name('exchange-details');
 
+Route::get('/page-not-found', function(){ return view('page-not-found');})->name('pagenotfound');
 
 //JSON responses
 Route::get('/gold-data/{id}', [DashboardController::class, 'getGoldData']);
