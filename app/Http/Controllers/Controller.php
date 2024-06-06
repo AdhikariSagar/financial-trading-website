097e6b303d55550adcbd07687b8fa179ebdfb425
Sagar Adhikari <sagar.adhikari86@gamil.com>
2024-06-07 00:22:44 +0200

    Add controller methods for showing instrument details in one view

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,60 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Exchange;
+use App\Models\Metadata;
+use Illuminate\Http\Request;
+
+class Controller
 {
-    //
+    public function instrumentDetails(Request $request)
+    {
+        $type = $this->getInstrumentType($request->type);
+        $symbol = $request->symbol;
+
+        if ($type !== "unknown") {
+            // Query to get metadata where both type and symbol match
+            $metaData = Metadata::where('type', $type)
+                ->where('symbol', $symbol)
+                ->first();
+
+            // Query to get exchange data where symbol matches
+            $exchangeData = Exchange::where('symbol', $symbol)->first();
+
+            // Check if any data is found
+            if ($metaData || $exchangeData) {
+                return view('instrument-details', compact('metaData', 'exchangeData'));
+            } else {
+                return redirect()->route('pagenotfound');
+            }
+        } else {
+            return redirect()->route('pagenotfound');
+        }
+        dd($type, $symbol);
+    }
+
+    // Helper function to determine the instrument type
+    private function getInstrumentType($type)
+    {
+        switch ($type) {
+            case "stock":
+                return "common stock";
+            case "cryptocurrency":
+                return "cryptocurrency";
+            case "etf":
+                return "exchange traded fund";
+            case "etc":
+                return "exchange traded commodity";
+            case "fund":
+                return "fund";
+            case "commodity":
+                return "commodity";
+            case "index":
+                return "index";
+            case "mutual-fund":
+                return "mutual fund";
+            default:
+                return "unknown";
+        }
+    }
 }
---
+++ resources/views/instrument-details.blade.php
@@ -0,0 +1,73 @@
+<x-main-layout>
+        <div>
+            <div>
+                <span class="text-3xl font-semibold">{{ $metaData->name }}</span>
+                <span class="text-3xl font-thin ml-2">{{ $metaData->ticker }}</span>
+            </div>
+            <div class="flex items-center mt-1">
+                <span
+                    class="h-8 flex items-center leading-none w-fit px-3 rounded-full bg-purple-600 text-white">Stock</span>
+                <span class="h-2 w-2 bg-gray-400 rounded-full mx-2"></span>
+            </div>
+        </div>
+        <div>{{ $metaData->description }}</div>
+        <div class="mt-8 h-full w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+            {{-- Graph container --}}
+            <div class="w-full lg:w-[65%]">
+                <div id="fetchId" data={{ $metaData->symbol }}></div>
+                <div class="w-full h-[600px] overflow-hidden">
+                    <div class="flex justify-center space-x-2 mt-4">
+                        <button onclick="updateChart('1D')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">1D</button>
+                        <button onclick="updateChart('5D')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">5D</button>
+                        <button onclick="updateChart('1M')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">1M</button>
+                        <button onclick="updateChart('3M')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">3M</button>
+                        <button onclick="updateChart('6M')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">6M</button>
+                        <button onclick="updateChart('1Y')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">1Y</button>
+                        <button onclick="updateChart('All')"
+                            class="btn w-8 h-6 rounded bg-green-300 hover:bg-green-200 cursor-pointer">Alle</button>
+                    </div>
+                    <canvas id="gold-price-chart" width="100%" height="100%"></canvas>
+                </div>
+            </div>
+            {{-- Other details --}}
+            <div class="w-full lg:w-[35%] md:space-y-10 md:pt-10">
+                <div>
+                    <h1 class="text-gray-500">Trading Information</h1>
+                    <div class="flex pb-2 border-b justify-between">
+                        <div>Previous Close Price</div>
+                        <div class="font-thin"> $424.01</div>
+                    </div>
+                    <div class="flex pb-2 border-b justify-between">
+                        <div>Previous Close Price</div>
+                        <div class="font-thin"> $424.01</div>
+                    </div>
+                    <div class="flex pb-2 border-b justify-between">
+                        <div>Previous Close Price</div>
+                        <div class="font-thin"> $424.01</div>
+                    </div>
+                </div>
+                <div>
+                    <h1 class="text-gray-500">Key Statistics</h1>
+                    <div class="flex pb-2 border-b justify-between">
+                        <div>Price/Earnings (Normalized)</div>
+                        <div class="font-thin"> 36.71</div>
+                    </div>
+                    <div class="flex pb-2 border-b justify-between">
+                        <div>Previous Close Price</div>
+                        <div class="font-thin"> $424.01</div>
+                    </div>
+                    <div class="flex pb-2 border-b justify-between">
+                        <div>Previous Close Price</div>
+                        <div class="font-thin"> $424.01</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+</x-main-layout>
