8cb6566bd52f4636d2f26ab31e739246a5dffec6
Sagar Adhikari <sagar.adhikari86@gamil.com>
2024-06-12 17:36:29 +0200

    chage in test cases

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,24 +5,21 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 //use Tests\TestCase;
 use Illuminate\Foundation\Testing\TestCase;
-
+/*
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+   
     use RefreshDatabase;
 
-    //public function test_the_application_returns_a_successful_response(): void
-    //{
-        //$response = $this->get('/');
-        //dd($response->getContent());
-        //dd($response);
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
 
         // Output the response for debugging
-        //$response->dump();
-
+    
         //$response->assertStatus(200);
         //$response->assertSee('Test Home Page'); // Adjust this assertion as per your response
-    //}
+    }
 }
+*/
